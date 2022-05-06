e0b74c89a7e31be3521b8ce2cb523accaf25ef88
Geoff Fox <fox.geoffrey@gmail.com>
2022-05-06 15:50:26 -0700

    Heal button

--- app.js
+++ app.js
@@ -30,12 +30,22 @@ const app = Vue.createApp({
       this.playerHealth -= attackValue;
     },
     specialAttackMonster() {
-        this.currentRound++;
-        const attackValue = getRandomValue(10, 25);
-        this.monsterHealth -= attackValue;
-        this.attackPlayer();
-        }
-    }
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    healPlayer() {
+      this.currentRound++;
+      const healValue = getRandomValue(8, 20);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      this.attackPlayer();
+    },
+  },
 });
 
 app.mount("#game");
